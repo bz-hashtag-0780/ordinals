7cdb4c233e79391ecb0a87ea0cb1fad7e0e363ab
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2023-12-04 16:58:41 +0100

    redirect to form

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: '/brc20',
+				destination: 'https://forms.gle/SNZbE5fzPd2mskkU7', // Replace with your actual Google Form URL
+				permanent: false, // Set to true if this redirect will be permanent
+			},
+			{
+				source: '/',
+				destination: 'https://forms.gle/SNZbE5fzPd2mskkU7', // Replace with your actual Google Form URL
+				permanent: false, // Set to true if this redirect will be permanent
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
